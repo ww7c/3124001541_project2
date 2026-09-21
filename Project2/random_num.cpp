67b7d2d14ebc7d8450c0990370a3152a7a34e237
schiessying985473-cmd <schiessying985473@gmail.com>
2026-09-21 16:50:50 +0800

    完成生成随机数模块

---
+++ Project2/random_num.cpp
@@ -0,0 +1,16 @@
+﻿#include "random_num.h"
+#include <random>
+using namespace std;
+
+int random_num::getnumber(int min, int max) {
+    static random_device rd;
+    static mt19937 gen(rd());
+
+    // 分布范围随参数动态生成
+    uniform_int_distribution<> dis(min, max);
+    return dis(gen);
+}
+//函数生成一个随机数，小于max
+int random_num::getnumber(int max) {
+    return getnumber(0, max - 1);
+}
---
+++ Project2/random_num.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+class random_num {
+public:
+	//生成自然数  （0<=num<max）
+	static int getnumber(int max);
+	//生成两个数之间的随机数  （min<=num<=max）
+	static int getnumber(int min, int max);
+};
