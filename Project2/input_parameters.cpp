89abc2b1291377423eedb01cf1390ebff59eb85c
schiessying985473-cmd <schiessying985473@gmail.com>
2026-09-21 16:50:14 +0800

    完成输入参数模块

---
+++ Project2/input_parameters.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+#include"input_parameters.h"
+using namespace std;
+void input_parameters::input()
+{
+	cout << "请输入题目数量:";
+	cin >> numbers;
+	cout << "请输入数字范围:";
+	cin >> maxnumbers;
+}
---
+++ Project2/input_parameters.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+class input_parameters {
+public:
+	int numbers;//题目数量
+	int maxnumbers;//最大数值
+	void input();//输入参数函数
+};
