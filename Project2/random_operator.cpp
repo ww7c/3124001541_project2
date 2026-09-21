7bbd7b464cb8cb4196980e0f7ee6b4affdc46d4d
schiessying985473-cmd <schiessying985473@gmail.com>
2026-09-21 16:51:22 +0800

    完成生成随机运算符模块

---
+++ Project2/random_operator.cpp
@@ -0,0 +1,16 @@
+﻿#include"random_operator.h"
+#include<random>
+using namespace std;
+string random_operator::getoperator()
+{
+	//运算符数组
+	static string operators[] = {
+		"+", "-", "*", "/"
+	};
+
+	static random_device rd;
+	static mt19937 gen(rd());
+	uniform_int_distribution<> dis(0, 3);
+	return operators[dis(gen)];
+}
+
---
+++ Project2/random_operator.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include<string>
+class random_operator {
+public:
+	//生成随机运算符
+	static std::string getoperator();
+};
